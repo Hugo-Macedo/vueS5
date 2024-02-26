fc9a4e0cc2f1a8e5e14827ac18e711f5353620d2
Hugo <macedohugo77@gmail.com>
2024-02-26 22:37:14 +0100

    feat: adding a login form

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Nav from '../components/Nav.vue';
+
 </script>
 
 <template>
   <main>
+    <Nav />
     <TheWelcome />
   </main>
 </template>
--- src/views/ProfilView.vue
+++ src/views/ProfilView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Profil from '../components/Profil.vue'
+import Nav from '../components/Nav.vue';
 </script>
 
 <template>
     <main>
+        <Nav />
         <Profil />
     </main>
 </template>
